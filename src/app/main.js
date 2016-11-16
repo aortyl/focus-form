4cdc739949a45646e10aa468ace8f79e184d43e5
Adam Ortyl <adam.ortyl@gmail.com>
2016-11-16 08:57:51 -0500

    Adding some stuff to the demo form.

--- src/app/main.js
+++ src/app/main.js
@@ -1,3 +1,8 @@
 export const main = {
-  template: require('./main.html')
+  template: require('./main.html'),
+  controller($scope) {
+    $scope.alert = function () {
+      $scope.alertMsg = 'Thanks! We sent your information off to the government for processing.';
+    };
+  }
 };
