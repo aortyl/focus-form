486604f38d6b38a48a615db0932a0110afa78a3d
Adam Ortyl <adam.ortyl@gmail.com>
2016-11-18 08:33:14 -0500

    Reworked the logic in the directive to handle all styling by adding/removing classes - no bools. Also got rid of anything 'gradient' named in the directive

--- src/app/focus-form.directive.js
+++ src/app/focus-form.directive.js
@@ -23,37 +23,55 @@ export const focusForm = function () {
                     $scope.focusOnChild(0, name, childCtrl);
                 }
 
-                // Set gradient on the second item
+                // Set focus class on the the next two sections if they get added
                 if ($scope.children.length === 2) {
-                    childCtrl.setGradient(true, 'down');
+                    childCtrl.setFocusClass('focus-plus-one');
+                } else if ($scope.children.length === 3) {
+                    childCtrl.setFocusClass('focus-plus-two');
                 }
 
                 return $scope.children.length - 1;
             };
 
-            $scope.focusOnChild = this.focusOnChild = function (index, name, childCtrl) {
-                // Remove focus from previous child (if it exists)
+            $scope.focusOnChild = this.focusOnChild = function (index) {
+                // Remove focus from previously focused section (if it exists)
                 if (angular.isDefined($scope.currentFocusIndex)) {
-                    // Remove gradients
-                    if ($scope.currentFocusIndex > 0) {
-                        $scope.children[$scope.currentFocusIndex - 1].controller.setGradient(false);
+                    if ($scope.currentFocusIndex - 2 >= 0) {
+                        $scope.children[$scope.currentFocusIndex - 2].controller.setFocusClass('');
                     }
-                    if ($scope.currentFocusIndex < $scope.children.length - 1) {
-                        $scope.children[$scope.currentFocusIndex + 1].controller.setGradient(false);
+                    if ($scope.currentFocusIndex - 1 >= 0) {
+                        $scope.children[$scope.currentFocusIndex - 1].controller.setFocusClass('');
                     }
 
-                    $scope.children[$scope.currentFocusIndex].controller.removeFocus();
+                    $scope.children[$scope.currentFocusIndex].controller.setFocusClass('');
+
+                    if ($scope.currentFocusIndex + 1 < $scope.children.length) {
+                        $scope.children[$scope.currentFocusIndex + 1].controller.setFocusClass('');
+                    }
+                    if ($scope.currentFocusIndex + 2 < $scope.children.length) {
+                        $scope.children[$scope.currentFocusIndex + 2].controller.setFocusClass('');
+                    }
                 }
-                // Set focus on new child
-                childCtrl.setFocus();
+
+                // Update classes of sections surrounding focused section
+                if (index - 2 >= 0) {
+                    $scope.children[index - 2].controller.setFocusClass('focus-minus-two');
+                }
+                if (index - 1 >= 0) {
+                    $scope.children[index - 1].controller.setFocusClass('focus-minus-one');
+                }
+
+                $scope.children[index].controller.setFocusClass('focus');
+
+                if (index + 1 < $scope.children.length) {
+                    $scope.children[index + 1].controller.setFocusClass('focus-plus-one');
+                }
+                if (index + 2 < $scope.children.length) {
+                    $scope.children[index + 2].controller.setFocusClass('focus-plus-two');
+                }
+
+                // Update current focus index
                 $scope.currentFocusIndex = index;
-
-                if (index > 0) {
-                    $scope.children[index - 1].controller.setGradient(true, 'up');
-                }
-                if (index < $scope.children.length - 1) {
-                    $scope.children[index + 1].controller.setGradient(true, 'down');
-                }
             };
         }
     };
@@ -77,42 +95,24 @@ export const focusFormSection = function () {
 
             scope.myIndex = scope.parentCtrl.addItem(scope.name, scope.myCtrl);
         },
-        controller($scope, $element, $timeout, $location, $anchorScroll) {
+        controller($scope, $log, $timeout, $location, $anchorScroll) {
             let timeout = null;
-            this.setFocus = function () {
-                $scope.focus = true;
-                if (timeout) {
-                    $timeout.cancel(timeout);
+            this.setFocusClass = function (focusClass) {
+                $scope.focusClass = focusClass;
+                if ($scope.focusClass === 'focus') {
+                    if (timeout) {
+                        $timeout.cancel(timeout);
+                    }
+                    timeout = $timeout(() => {
+                        $location.hash($scope.name);
+                        $anchorScroll();
+                    }, 0);
                 }
-                timeout = $timeout(() => {
-                    $location.hash($scope.name);
-                    $anchorScroll();
-                }, 0);
-                $scope.gradientUp = false;
-                $scope.gradientDown = false;
-            };
-
-            this.removeFocus = function () {
-                $scope.focus = false;
+                $log.debug('focus class', $scope.focusClass);
             };
 
             $scope.focusMe = function () {
                 $scope.parentCtrl.focusOnChild($scope.myIndex, $scope.name, $scope.myCtrl);
-            };
-
-            this.setGradient = function (useGradient, direction) {
-                if (useGradient) {
-                    if (direction === 'up') {
-                        $scope.gradientUp = true;
-                        $scope.gradientDown = false;
-                    } else if (direction === 'down') {
-                        $scope.gradientUp = false;
-                        $scope.gradientDown = true;
-                    }
-                } else {
-                    $scope.gradientUp = false;
-                    $scope.gradientDown = false;
-                }
             };
         }
     };
